d5f524c2849d4ec36f51db52ea4f144fbea93ed1
Angelo Zero <angelo.akm@gmail.com>
2024-09-08 17:10:58 -0300

    spring security 6.4

---
+++ security64/src/main/java/com/angelozero/spring/security64/Security64Application.java
@@ -0,0 +1,13 @@
+package com.angelozero.spring.security64;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Security64Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Security64Application.class, args);
+	}
+
+}
